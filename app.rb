89f40c2eed106d213fb0a096efd3caa3fb12c3d3
brighton <brightonspak@gmail.com>
2022-10-16 23:59:21 +0300

    complete tests

---
+++ app.rb
@@ -0,0 +1,13 @@
+# app.rb
+#puts "Hello world!"
+# app.rb
+# print "Hello world!"
+# app.rb
+#puts [1, 2, 3]
+#p [1, 2, 3]
+# app.rb
+#pp [ { id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" } ]
+
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
